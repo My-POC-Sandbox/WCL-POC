1217e830880dfdbc555da2ff00b19110d1443c52
Rodrigo Lopes <rlopes@mgmresorts.com>
2021-03-23 15:50:11 -0300

    chore: created the packages

---
+++ packages/wcl-core/index.ts
@@ -0,0 +1,5 @@
+import { value as iconValue } from '@my-poc-sandbox/wcl-icons';
+import { value as stylesValue } from '@my-poc-sandbox/wcl-styles';
+import { value as utilsValue } from '@my-poc-sandbox/wcl-utils';
+
+export const value = iconValue + stylesValue + utilsValue;
